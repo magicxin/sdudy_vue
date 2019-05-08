e49db9d1a6693bad357d3ce9edcd93e11de7605a
magicxin <xin_sui@yahoo.com>
2019-05-08 17:15:35 +0800

    broswerify init

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,20 +19,15 @@ module.exports = function(grunt) {
           },
         },
       },
-      componentbuild: {
-        options: {
-          src: 'index.css',
-          dest: './build/',
-          name: 'study_vue',
-          verbose: true
-        }
+      browserify: {
+        'build/bundle.js': ['src/main.js']
       }
   });
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks( 'grunt-contrib-jshint' );
     grunt.loadNpmTasks( 'grunt-mocha' );
-    grunt.loadNpmTasks('grunt-component-build');
+    grunt.loadNpmTasks('grunt-browserify');
     // 默认被执行的任务列表。
     grunt.registerTask('test',['mocha']);
-    grunt.registerTask('default',['jshint','mocha','componentbuild']);
+    grunt.registerTask('default',['jshint','mocha','browserify']);
   };
